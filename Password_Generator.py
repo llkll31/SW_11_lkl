2cfd15394482cadb2546283924d03b40135a7450
HongRae-Kim <20217142@hallym.ac.kr>
2024-11-14 10:41:42 +0900

    first commit

---
+++ Password_Generator.py
@@ -0,0 +1,21 @@
+import random 
+
+print('Welcom To Your Password Generator')
+
+chars = 'abcdefgjustghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*().,?0123456789'
+
+number = input('Amount of passwords to generate: ')
+number = int(number)
+
+length = input('Input your password length: ')
+length = int(length)
+
+print('\nhere are your password: ')
+
+for pwd in range(number):
+    passwords = ''
+    for c in range(length):
+        passwords += random.choice(chars)
+    print(passwords)
+
+
